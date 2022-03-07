716789a189dd9a335df566eeb7baadabdabdcf68
Garikoitz <garikoitz@gmail.com>
2022-03-07 22:24:34 +0100

    updating word lists

--- word_functions/WordImages_Create.m
+++ word_functions/WordImages_Create.m
@@ -1,6 +1,6 @@
-function [] = WordImages_Create(WordPath, imFileType)
+function [] = WordImages_Create(bdir, imFileType)
 %{
-WordPath = '~/Downloads/VWFA';
+bdir = fullfile(vlRP,'DATA');
 imFileType = 'png';
 
 WordImages_Create(WordPath, imFileType)
@@ -12,81 +12,74 @@ WordImages_Create(WordPath, imFileType)
 %% Housekeeping
 % clear all; close all; clc;
 % WordPath = pwd; % Run it in Stimuli/SoS or Stimuli/ajencia
-ImgWritePath = [WordPath, filesep, 'TASKgray']; % GREY60 son solo para Ajencia
+WordPath     = fullfile(bdir,'output');
+ImgWritePath = fullfile(bdir, 'images'); 
 if ~isfolder(ImgWritePath); mkdir(ImgWritePath); end
-% imFileType = 'png';
+
 % matlabpool ips_base; 
 
-%% Leer archivos que nos interesan
-% Ojo con la codificacion, si no es iso8859 no va bien
-% arreglar archivos con esto
-% iconv -t UTF-8 -f ISO-8859-1 espal_orig.txt > espal_orig_utf8.txt
-% Ademas para saber la codificacion usar file -bi filename.txt
-cd(WordPath);
-if IsLinux
-    filesWithRW = dir('RW_*soloword.txt');
-    filesWithPW = dir('PW_*soloword.txt');
+%% Read only word files
+filesWithRW = dir(fullfile(WordPath,'RW_*justwords.txt'));
+filesWithPW = dir(fullfile(WordPath,'PW_*justwords.txt'));
+
+RW       = table();
+colnames = {'LANG','TYPE','CB','WORD','cWORD'};
+N        = 80;
+   
+for nfile = 1:length(filesWithRW)
+    fileID     = fopen(fopen(fullfile(WordPath,filesWithRW(nfile).name), 'r', 'n','utf-8'));
+    [words]    = textread(fileID,'%s%*[^\n]', 'headerlines', 0);
+    tmpt       = table();
+    tmpt.WORD  = categorical(words);
+    tmpt.LANG  = categorical(repmat({filesWithRW(nfile).name(4:5)},[N,1]));
+    tmpt.TYPE  = categorical(repmat({filesWithRW(nfile).name(1:2)},[N,1]));
+    tmpt.CB    = categorical(repmat({filesWithRW(nfile).name(7:9)},[N,1]));
+    tmpt.cWORD = categorical(cellfun(@CambiaTilde, words,'UniformOutput', false));
+    
+    % Concatenate with the rest of the files
+    RW = [RW;tmpt];
 end
-% If you want to take all of them
-% Tener esto comentado, leo todas las palabras para crear los task
-% filesWithRW = dir('DesdeBASP_UTF8.txt');
-% fileID = fopen(fopen(['iso_' filesWithRW(1).name], 'r', 'n', 'ISO-8859-1'));
-% [words] = textread(fileID,'%s%*[^\n]', 'headerlines', 1);
-% filesWithRW = dir('soloword_ehitz_utf8_ajenciaYlodeSoS_arreglado.txt');
-% filesWithRW = dir('nueva_lista_con_60.txt');
-% filesWithRW = dir('sinusar_Length456.txt');
-% filesWithPW = dir('sinusar_PW_Length456.txt');
 
-%% Clean words from tildes etc. It has to be done in Linux, it doesn't work in Mac.
-% Then I save a .mat and open it in mac in order to create the Georgian
-% bmp images.
-
-if IsLinux
-    RW_group = cell(size(filesWithRW));
-    RW_list = cell(size(filesWithRW));
-    clean_RW = cell(size(filesWithRW));
-    for nfile = 1:length(filesWithRW)
-        %system(['iconv -f UTF-8 -t ISO-8859-1 ' filesWithRW(nfile).name ...
-        %    ' > iso_' filesWithRW(nfile).name]); 
-        % fileID = fopen(fopen(['iso_' filesWithRW(nfile).name], 'r', 'n',...
-        %     'ISO-8859-1'));
-        fileID = fopen(fopen([filesWithRW(nfile).name], 'r', 'n','utf-8'));
-        [words] = textread(fileID,'%s%*[^\n]', 'headerlines', 0);
-        cleanwords = words;
-        for j = 1:length(cleanwords) 
-            	cleanwords{j} = CambiaTilde(cleanwords{j});
-        end
-        RW_group{nfile} = filesWithRW(nfile).name(4:6);
-        RW_list{nfile} = words;
-        clean_RW{nfile} = cleanwords;
-    end
-
-    save('cleanwords_for_georgian_80t.mat', 'RW_group', 'clean_RW');
+PW       = table();
+colnames = {'LANG','TYPE','CB','WORD','cWORD'};
+N        = 80;
+   
+for nfile = 1:length(filesWithPW)
+    fileID     = readtable(fullfile(WordPath,filesWithPW(nfile).name));
+    [words]    = fileID.Match;
+    tmpt       = table();
+    tmpt.WORD  = categorical(words);
+    tmpt.LANG  = categorical(repmat({filesWithRW(nfile).name(4:5)},[N,1]));
+    tmpt.TYPE  = categorical(repmat({filesWithRW(nfile).name(1:2)},[N,1]));
+    tmpt.CB    = categorical(repmat({filesWithRW(nfile).name(7:9)},[N,1]));
+    tmpt.cWORD = categorical(cellfun(@CambiaTilde, words,'UniformOutput', false));
+    
+    % Concatenate with the rest of the files
+    PW = [PW;tmpt];
 end
 
 %% Now we can write the Real Words + Phase Scrambled + Scrambled + CB
-if IsLinux
-    for nfile = 1:length(RW_group)
-        group = RW_group{nfile};
-        [words] = RW_list{nfile};
-        [cleanwords] = clean_RW{nfile};
-        
-        WordFilenames = strcat(group, '_', cleanwords, '.', imFileType);
-
-        parfor i = 1:length(words)
-            mPalabra = char(words(i));
-            mImFilename = char(WordFilenames(i));
-            mFont = 'Arial';
-            mFont_Size = 12;
-            mSamplesPerPoint = 8;
-            mPhaseScrambleW = 1;
-            mTipo = 'all';  % 'all' o 'solo_im'
-            SampleImgWritePath = ImgWritePath;
-            % LLamada a la funcion
-            RenderWriteWord(mPalabra, mFont, mFont_Size, mSamplesPerPoint, mTipo, ImgWritePath, mImFilename, mPhaseScrambleW, imFileType);
-        end
+for nfile = 1:length(RW_group)
+    group        = RW_group{nfile};
+    [words]      = RW_list{nfile};
+    [cleanwords] = clean_RW{nfile};
+    
+    WordFilenames = strcat(group, '_', cleanwords, '.', imFileType);
+    
+    parfor i = 1:length(words)
+        mPalabra = char(words(i));
+        mImFilename = char(WordFilenames(i));
+        mFont = 'Arial';
+        mFont_Size = 12;
+        mSamplesPerPoint = 8;
+        mPhaseScrambleW = 1;
+        mTipo = 'all';  % 'all' o 'solo_im'
+        SampleImgWritePath = ImgWritePath;
+        % LLamada a la funcion
+        RenderWriteWord(mPalabra, mFont, mFont_Size, mSamplesPerPoint, mTipo, ImgWritePath, mImFilename, mPhaseScrambleW, imFileType);
     end
 end
+
 
 %% Here we generate the pseudoword images
 if IsLinux
@@ -175,7 +168,7 @@ end
 % and everything and then remove the tildes before converting them to japanese
 % characters
 if ismac
-    cd(WordPath)
+
     W = readtable('RW_HF2Length456_80.txt', 'VariableNamingRule','preserve');
     W.Properties.VariableNames = strrep(W.Properties.VariableNames,'|','_');
     % Clean words from tildes
